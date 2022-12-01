b68b0679ad07774748f93d93a4d96847fc7a8691
k8s <k8s@scott-tap-jump.tanzu.lab>
2022-12-01 13:33:43 +0000

    basic vcluster lab

--- workshop/content/workshop-overview.md
+++ workshop/content/workshop-overview.md
@@ -1,1 +1,6 @@
-# FILL ME IN WITH CONTENT
+# K8s Playground
+This lab environment includes an integrated VSCode editor, an embedded octant dashboard and 2 terminals for your use.  
+
+We have setup a fully functional K8s cluster for this lab using vCluster and you have full cluster-admin permissions on the vcluster kube context!
+
+Have fun and learn k8s!
--- workshop/setup.d/setup-resources.sh
+++ workshop/setup.d/setup-resources.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-echo "put pre launch commands here"
+curl -L --silent --show-error --fail -o /opt/kubernetes/bin/vcluster https://github.com/loft-sh/vcluster/releases/latest/download/vcluster-linux-amd64 --retry 10 --retry-connrefused --retry-max-time 120 --retry-delay 5
+chmod +x /opt/kubernetes/bin/vcluster
+vcluster connect my-vcluster -n $SESSION_NAMESPACE-vc --server https://my-vcluster.$SESSION_NAMESPACE-vc.svc.cluster.local --kube-config-context-name vcluster
+curl -L --silent --show-error --fail -o /tmp/carvel.sh https://carvel.dev/install.sh && \
+    export K14SIO_INSTALL_BIN_DIR=/opt/kubernetes/bin && \
+    bash -x /tmp/carvel.sh && \
+    rm /tmp/carvel.sh
